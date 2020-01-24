4ad9a9b2e27af6e038fb26178272520ef585d7e2
Daniel Mattox <dmattox10@gmail.com>
2020-01-24 11:30:51 -0500

    Tabbed layout, placeholder components

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { Container } from 'reactstrap'
+import { Container, Nav, NavItem, NavLink, TabContent, TabPane, Row, Col } from 'reactstrap'
+import classnames from 'classnames';
+
+import Add from './components/Add'
+import Display from './components/Display'
 
 import { SmurfContext } from './contexts/SmurfContext'
 
@@ -11,6 +15,11 @@ const App = () => {
   
     const [smurfs, addSmurf, updateSmurf, deleteSmurf] = useVillage()
 
+    const [activeTab, setActiveTab] = useState('1');
+    const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);
+    }
+
     return (
       <div className='App'>
         <SmurfContext.Provider value={{smurfs, addSmurf, updateSmurf, deleteSmurf}}>
@@ -18,7 +27,40 @@ const App = () => {
             <Population />
           <div className='spacer'></div>
           <Container>
-
+            <Nav tabs>
+              <NavItem className='half'>
+                <NavLink
+                  className={classnames({ active: activeTab === '1' })}
+                  onClick={() => { toggle('1'); }}
+                >
+                  Add
+                </NavLink>
+              </NavItem>
+              <NavItem className='half'>
+                <NavLink
+                  className={classnames({ active: activeTab === '2' })}
+                  onClick={() => { toggle('2'); }}
+                >
+                  Display
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  <Col sm="12">
+                    <Add />
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col sm="12">
+                    <Display />
+                  </Col>
+                </Row>
+              </TabPane>
+            </TabContent>
           </Container>
         </SmurfContext.Provider>
       </div>
---
+++ smurfs/src/components/Add.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <h1>Add A Smurf</h1>
+    )
+}
+
+export default Add
